a933749d8ad5bfdccc3a46a0293141f54aa2b735
Kelley van Evert <hello@kelleyvanevert.nl>
2019-06-01 19:07:36 +0200

    Updates

---
+++ precache-manifest.fc769ea25dcfca9ab72a9154ec1d5467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b162678feaeef6fc8fffa52bb0d12bb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a61d85410c7857fea6",
+    "url": "/static/css/2.408da795.chunk.css"
+  },
+  {
+    "revision": "80fb01944b8ca483755b",
+    "url": "/static/css/main.52258923.chunk.css"
+  },
+  {
+    "revision": "c2a61d85410c7857fea6",
+    "url": "/static/js/2.2482bf78.chunk.js"
+  },
+  {
+    "revision": "80fb01944b8ca483755b",
+    "url": "/static/js/main.864391b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e0c3fb5fe72859e528a4e920fc8f29db",
+    "url": "/static/media/logo.e0c3fb5f.svg"
+  }
+]);
